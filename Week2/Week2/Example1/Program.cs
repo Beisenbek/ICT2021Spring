75fedad9d1d04f99380a89f2ff2784f23b42eda1
Beisenbek Baisakov <beysenbek@gmail.com>
2021-02-06 18:24:51 +0600

    upd

---
+++ Week2/Week2/Example1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F4();
+        }
+
+
+        static void PrintFolderInfo(string path, string prefix = "")
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            try
+            {
+                foreach (FileSystemInfo fi in dir.GetFileSystemInfos())
+                {
+                    Console.WriteLine(prefix + " " + fi.Name);
+                    if (fi.GetType() == typeof(DirectoryInfo))
+                    {
+                        PrintFolderInfo(fi.FullName, prefix + "--");
+                    }
+                }
+            }
+            catch (Exception) { }
+        }
+        private static void F4()
+        {
+            PrintFolderInfo(@"C:\Users\bsnbk\Documents");   
+        }
+
+        private static void F3()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\bsnbk\Documents\pp1\samples\week1\day2 g1");
+            foreach (FileSystemInfo fi in dir.GetFileSystemInfos())
+            {
+                Console.WriteLine(fi.Name);
+            }
+        }
+
+        private static void F2()
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\bsnbk\Documents\pp1\samples\week1\day2 g1");
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                Console.WriteLine(d.Name);
+            }
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                Console.WriteLine(f.Name);
+            }
+        }
+
+        private static void F1()
+        {
+            //FileSystemInfo
+            //FileInfo
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\bsnbk\Documents\pp1\samples\week1");
+
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                Console.WriteLine(d.Name);
+            }
+        }
+
+        private static void F0()
+        {
+            foreach (string l in Environment.GetLogicalDrives())
+            {
+                Console.WriteLine(l);
+            }
+        }
+    }
+}
---
+++ Week2/Week2/Example2/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Example2
+{
+    class Layer
+    {
+        public DirectoryInfo dir
+        {
+            get;
+            set;
+        }
+        public int pos
+        {
+            get;
+            set;
+        }
+        public List<FileSystemInfo> content
+        {
+            get;
+            set;
+        }
+
+        public Layer(DirectoryInfo dir, int pos)
+        {
+            this.dir = dir;
+            this.pos = pos;
+            this.content = new List<FileSystemInfo>();
+
+
+            content.AddRange(this.dir.GetDirectories());
+            content.AddRange(this.dir.GetFiles());
+        }
+
+        public void PrintInfo()
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            int cnt = 0;
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(d.Name);
+                cnt++;
+            }
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            foreach (FileInfo f in dir.GetFiles())
+            {
+                if (cnt == pos)
+                {
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                Console.WriteLine(f.Name);
+                cnt++;
+            }
+        }
+
+        public FileSystemInfo GetCurrentObject()
+        {
+            return content[pos];
+        }
+
+        public void SetNewPosition(int d)
+        {
+            if (d > 0)
+            {
+                pos++;
+            }
+            else
+            {
+                pos--;
+            }
+            if (pos >= content.Count)
+            {
+                pos = 0;
+            }else if(pos < 0)
+            {
+                pos = content.Count - 1;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F3();
+        }
+
+        private static void F3()
+        {
+
+            Stack<Layer> history = new Stack<Layer>();
+            history.Push(new Layer(new DirectoryInfo(@"C:\"), 0 ));
+
+            bool escape = false;
+
+            while (!escape)
+            {
+                Console.Clear();
+
+                history.Peek().PrintInfo();
+
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey(true);
+
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.Enter:
+                        if(history.Peek().GetCurrentObject().GetType() == typeof(DirectoryInfo))
+                        {
+                            history.Push(new Layer(history.Peek().GetCurrentObject() as DirectoryInfo, 0));
+                        }
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SetNewPosition(-1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SetNewPosition(1);
+                        break;
+                    case ConsoleKey.Escape:
+                        history.Pop();
+                        break;
+                }
+            }
+        }
+
+        private static void F2()
+        {
+            while (true)
+            {
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                if (consoleKeyInfo.Key == ConsoleKey.Escape) break;
+                Console.WriteLine(consoleKeyInfo.KeyChar);
+            }
+        }
+
+        private static void F1()
+        {
+            ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+            Console.WriteLine(consoleKeyInfo);
+        }
+    }
+}
