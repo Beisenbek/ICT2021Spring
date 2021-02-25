1971634e4715b4ba259b5b8ad7b3736b73a9949c
Beisenbek Baisakov <b.baisakov@kbtu.kz>
2021-02-24 23:45:53 -0800

    'upd'

---
+++ Week5/TestApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TestApp
+{
+    class Student
+    {
+
+        public string name;
+        public string aname;
+
+        public override string ToString()
+        {
+            return name + " " + aname;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.aname = "Smith";
+            s.name = "Bob";
+
+            Console.WriteLine(s);
+        }
+    }
+}
