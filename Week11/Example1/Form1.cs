3a91aebb8cf0a5ebabfd44f03aa498457cafc5bc
Beisenbek Baisakov <b.baisakov@kbtu.kz>
2021-04-10 18:21:03 +0600

    'upd'

--- Week11/Example1/Form1.cs
+++ Week11/Example1/Form1.cs
@@ -26,7 +26,7 @@ namespace Example1
         Point currentPoint = default(Point);
         Graphics gfx = default(Graphics);
         Pen p = new Pen(Color.Black);
-        //0 - pen, 1 - fill
+        //0 - pen, 1 - fill, 2 - gdi32 fill
         int currentTool = 0;
         Bitmap bmp = default(Bitmap);
 
@@ -69,7 +69,15 @@ namespace Example1
             if (currentTool == 1)
             {
                 currentPoint = e.Location;
-                bmp = Utils.Fill(bmp, currentPoint, bmp.GetPixel(e.X, e.Y), Color.Red);
+                bmp = Utils.Fill(bmp, currentPoint, bmp.GetPixel(e.X, e.Y), Color.Blue);
+                gfx = Graphics.FromImage(bmp);
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+            }
+            else if (currentTool == 2)
+            {
+                MapFill mf = new MapFill();
+                mf.Fill(gfx, currentPoint, Color.Red, ref bmp);
                 gfx = Graphics.FromImage(bmp);
                 pictureBox1.Image = bmp;
                 pictureBox1.Refresh();
@@ -89,6 +97,11 @@ namespace Example1
         private void button2_Click(object sender, EventArgs e)
         {
             currentTool = 1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            currentTool = 2;
         }
     }
 }
---
+++ Week11/Example2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace Example2
+{
+    public partial class Form1 : Form
+    {
+        public void F2()
+        {
+            string cs = @"URI=file:C:\Users\bsnbk\Downloads\chinook\chinook.db";
+            using (var con = new SQLiteConnection(cs))
+            {
+                con.Open();
+                var cmd = new SQLiteCommand(con);
+                cmd.CommandText = "DROP TABLE IF EXISTS cars";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = "CREATE TABLE cars(id INTEGER PRIMARY KEY, name TEXT, price INT)";
+                cmd.ExecuteNonQuery();
+                
+                cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',123)";
+                
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("OK!");
+            }
+
+        }
+
+        public void F1()
+        {
+            string cs = "Data Source = :memory:";
+            string stm = "SELECT SQLITE_VERSION()";
+
+            var con = new SQLiteConnection(cs);
+
+            con.Open();
+
+            var cmd = new SQLiteCommand(stm, con);
+
+            string res = cmd.ExecuteScalar().ToString();
+
+            MessageBox.Show(res);
+
+            con.Close();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            F2();
+
+        }
+    }
+}
