65bf3c601ae71db7709ca1d1c76cc061e6e93f05
Beisenbek Baisakov <b.baisakov@kbtu.kz>
2021-04-10 16:50:25 +0600

    'upd'

---
+++ Week11/Example1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gfx = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+            gfx.Clear(Color.White);
+        }
+
+        bool isMouseDown = false;
+        Point prevPoint = default(Point);
+        Point currentPoint = default(Point);
+        Graphics gfx = default(Graphics);
+        Pen p = new Pen(Color.Black);
+        //0 - pen, 1 - fill
+        int currentTool = 0;
+        Bitmap bmp = default(Bitmap);
+
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                if (currentTool == 0)
+                {
+                    prevPoint = currentPoint;
+                    currentPoint = e.Location;
+                    gfx.DrawLine(p, prevPoint, currentPoint);
+                    pictureBox1.Refresh();
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            currentPoint = e.Location;
+            if (currentTool == 1)
+            {
+                currentPoint = e.Location;
+                bmp = Utils.Fill(bmp, currentPoint, bmp.GetPixel(e.X, e.Y), Color.Red);
+                gfx = Graphics.FromImage(bmp);
+                pictureBox1.Image = bmp;
+                pictureBox1.Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            currentTool = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            currentTool = 1;
+        }
+    }
+}
---
+++ Week11/Example1/Utils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Utils
+    {
+        static void Step(
+            int x,
+            int y,
+            int w,
+            int h,
+            Bitmap bmp,
+            Color originColor,
+            Color fillColor,
+            Queue<Point> q)
+        {
+            if (x >= 0 && y >= 0 && x < w && y < h)
+            {
+                if (bmp.GetPixel(x, y) == originColor)
+                {
+                    bmp.SetPixel(x, y, fillColor);
+                    q.Enqueue(new Point(x, y));
+                }
+            }
+        }
+        public static Bitmap Fill(
+            Bitmap bmp,
+            Point originPoint,
+            Color originColor,
+            Color fillColor)
+        {
+            Queue<Point> q = new Queue<Point>();
+            bmp.SetPixel(originPoint.X, originPoint.Y, fillColor);
+            q.Enqueue(originPoint);
+            while (q.Count > 0)
+            {
+                Point cur = q.Dequeue();
+                Step(cur.X + 1, cur.Y, bmp.Width, bmp.Height, bmp, originColor, fillColor, q);
+                Step(cur.X - 1, cur.Y, bmp.Width, bmp.Height, bmp, originColor, fillColor, q);
+                Step(cur.X, cur.Y + 1, bmp.Width, bmp.Height, bmp, originColor, fillColor, q);
+                Step(cur.X, cur.Y - 1, bmp.Width, bmp.Height, bmp, originColor, fillColor, q);
+            }
+            return bmp;
+        }
+    }
+}
